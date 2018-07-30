afcd546c2955561e817a4d5606ec059f9ea1d269
Jesse Jaggars <jhjaggars@gmail.com>
2018-07-30 14:38:20 -0500

    supporting remove, sorting, and done

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,28 @@ use clap::{Arg, App, SubCommand};
 use chrono::prelude::*;
 use chrono_humanize::HumanTime;
 use rand::{thread_rng, Rng};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::fs;
+use std::iter::FromIterator;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TodoList {
-    items: Vec<TodoItem>,
+    items: HashMap<char, TodoItem>,
 }
 
 impl TodoList {
     fn new() -> TodoList {
-        TodoList { items: Vec::new() }
+        TodoList { items: HashMap::new() }
     }
 
     fn read(path: &str) -> TodoList {
         match fs::read(path) {
             Ok(contents) => {
                 TodoList {
-                    items: serde_json::from_str(&String::from_utf8_lossy(&contents)).unwrap()
+                    items: serde_json::from_str(&String::from_utf8_lossy(&contents)).unwrap(),
                 }
-            },
-            Err(_) => TodoList::new()
+            }
+            Err(_) => TodoList::new(),
         }
     }
 
@@ -40,7 +41,7 @@ impl TodoList {
         fs::write(path, serde_json::to_string_pretty(&self.items).unwrap()).unwrap();
     }
 
-    fn add_many(&mut self, items: Vec<&str>) {
+    fn add_many(&mut self, items: &Vec<&str>) {
         for i in items {
             self.add(i);
         }
@@ -48,7 +49,7 @@ impl TodoList {
 
     fn add(&mut self, description: &str) {
         let index = self.get_next_index();
-        self.items.push(TodoItem::new(description, index));
+        self.items.insert(index, TodoItem::new(description, index));
     }
 
     fn get_next_index(&self) -> char {
@@ -56,22 +57,56 @@ impl TodoList {
             "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
         ).chars()
             .collect();
-        let indices = self.items
-            .iter()
-            .map(|item| item.index)
-            .collect::<HashSet<char>>();
+        let indices = self.items.keys().cloned().collect();
         let choices: Vec<char> = possibles
             .difference(&indices)
             .cloned()
             .collect::<Vec<char>>();
+        if choices.len() < 1 {
+            panic!(
+                "No more item indices remain!  You need to remove some things before adding more!"
+            );
+        }
         let mut rng = thread_rng();
         let choice: char = *rng.choose(&choices).unwrap();
         return choice;
     }
 
-    fn show(self) {
-        for i in self.items {
-            println!("{} {} created {}", i.index, i.description, HumanTime::from(i.created));
+    fn show(&self, by: &str) {
+        let mut items = Vec::from_iter(self.items.values());
+        items.sort_by_key(|item| item.attr(by));
+        for i in items {
+            println!(
+                "{} {} created {}",
+                i.index,
+                i.description,
+                HumanTime::from(i.created)
+            );
+        }
+    }
+
+    fn remove(&mut self, index: &str) {
+        let item = self.items.remove(&index.chars().next().unwrap());
+        match item {
+            None => println!("Couldn't find an item at index '{}' to remove.", index),
+            Some(i) => println!("Ok. Removing item {} '{}'", index, i.description),
+        }
+    }
+
+    fn remove_many(&mut self, items: &Vec<&str>) {
+        for i in items {
+            self.remove(i);
+        }
+    }
+
+    fn done_many(&mut self, items: &Vec<&str>) {
+        for i in items {
+            let ch = i.chars().next().unwrap();
+            let item = self.items.get_mut(&ch);
+            match item {
+                Some(el) => el.done = !el.done,
+                None => println!("No item at index '{}'.", ch),
+            }
         }
     }
 }
@@ -94,6 +129,16 @@ impl TodoItem {
             done: false,
             created: now,
             updated: now,
+        }
+    }
+
+    fn attr(&self, key: &str) -> String {
+        match key {
+            "index" => self.index.to_string(),
+            "done" => self.done.to_string(),
+            "created" => self.created.to_string(),
+            "updated" => self.updated.to_string(),
+            _ => self.created.to_string(),
         }
     }
 }
@@ -138,7 +183,7 @@ fn main() {
         Some("show") => {
             let by = matches.value_of("by").unwrap_or("done");
             let todo_list = TodoList::read("tl.json");
-            todo_list.show();
+            todo_list.show(by);
         }
         Some("add") => {
             let items: Vec<&str> = matches
@@ -148,9 +193,31 @@ fn main() {
                 .unwrap()
                 .collect();
             let mut todo_list = TodoList::read("tl.json");
-            todo_list.add_many(items);
+            todo_list.add_many(&items);
             todo_list.write("tl.json");
-            println!("{}", serde_json::to_string_pretty(&todo_list).unwrap());
+            println!("Ok, added {} items.", items.len());
+        }
+        Some("remove") => {
+            let items: Vec<&str> = matches
+                .subcommand_matches("remove")
+                .unwrap()
+                .values_of("index")
+                .unwrap()
+                .collect();
+            let mut todo_list = TodoList::read("tl.json");
+            todo_list.remove_many(&items);
+            todo_list.write("tl.json");
+        }
+        Some("done") => {
+            let items: Vec<&str> = matches
+                .subcommand_matches("done")
+                .unwrap()
+                .values_of("index")
+                .unwrap()
+                .collect();
+            let mut todo_list = TodoList::read("tl.json");
+            todo_list.done_many(&items);
+            todo_list.write("tl.json");
         }
         None => (),
         _ => (),
